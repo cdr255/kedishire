28c818ae39667baf45426a7d8e52423a5e7790a2
Christopher Rodriguez <cdr255@gmail.com>
2018-06-22 00:46:07 -0400

    Basic Compiling and Linking done.

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+int main(int argc, char ** argv)
+{
+  return 0;
+}
